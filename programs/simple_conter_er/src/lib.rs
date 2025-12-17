457ba2917d0116b8f212cf4fb17fcf9453d75a10
alizeeshan1234 <mohdalizeeshan4@gmail.com>
2025-12-17 21:28:36 +0400

    new er counter

---
+++ programs/simple_conter_er/src/lib.rs
@@ -0,0 +1,121 @@
+use anchor_lang::prelude::*;
+
+declare_id!("EKpNvKTXRa3b8qHY9WgJaeVpRBqiELRm2PTygRB3CMLG");
+
+use ephemeral_rollups_sdk::anchor::{commit, delegate, ephemeral};
+use ephemeral_rollups_sdk::cpi::DelegateConfig;
+
+#[ephemeral]
+#[program]
+pub mod simple_conter_er {
+    use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<InitializeCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment_counter(ctx: Context<IncrementCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented successfully!");
+        msg!("Counter Value: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn delegate_counter(ctx: Context<DelegateCounter>) -> Result<()> {
+        let delegate_config = DelegateConfig {
+            commit_frequency_ms: 30_000,
+            validator: Some(pubkey!("MAS1Dt9qreoRMQ14YQuhg8UTZMMzDdKhmkZMECCzk57")),
+        };
+
+        let seed: &[u8] = b"counter";
+        let seeds = &[seed];
+
+        ctx.accounts.delegate_counter(&ctx.accounts.signer, seeds, delegate_config)?;
+
+        msg!("Delegated counter account successfully!");
+
+        Ok(())
+    }
+
+    pub fn commit_counter(ctx: Context<CommitCounter>) -> Result<()> {
+        commit_and_undelegate_accounts(
+            &ctx.accounts.signer, 
+            vec![&ctx.accounts.counter.to_account_info()], 
+            &ctx.accounts.magic_context, 
+            &ctx.accounts.magic_program
+        )?;
+
+        msg!("Commited Accounts SUccessfully!");
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+pub struct InitializeCounter<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct IncrementCounter<'info> {
+     #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[delegate]
+#[derive(Accounts)]
+pub struct DelegateCounter<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        del,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[commit]
+#[derive(Accounts)]
+pub struct CommitCounter<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64
+}
---
+++ tests/simple_conter_er.ts
@@ -0,0 +1,109 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SimpleConterEr } from "../target/types/simple_conter_er";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import {
+  GetCommitmentSignature,
+  MAGIC_CONTEXT_ID,
+  MAGIC_PROGRAM_ID
+} from "@magicblock-labs/ephemeral-rollups-sdk";
+import { sendMagicTransaction } from "magic-router-sdk";
+import { web3 } from "@coral-xyz/anchor";
+
+describe("simple_conter_er", () => {
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.simpleConterEr as Program<SimpleConterEr>;
+
+  let counterAccount: PublicKey;
+
+  const routerConnection = new web3.Connection(
+    process.env.ROUTER_ENDPOINT || "https://devnet-router.magicblock.app",
+    {
+      wsEndpoint: process.env.ROUTER_WS_ENDPOINT || "wss://devnet-router.magicblock.app",
+    }
+  );
+
+  before(async () => {
+    [counterAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("counter")],
+      program.programId
+    )
+  })
+
+  it("initialized counter", async () => {
+    const tx = await program.methods.initialize().accountsPartial({
+      signer: provider.wallet.publicKey,
+      counter: counterAccount,
+      systemProgram: SystemProgram.programId,
+    }).signers([provider.wallet.payer]).rpc();
+
+    console.log(`Transction Singnature: ${tx}`);
+  });
+
+  it("increment counter on chain", async () => {
+    const tx = await program.methods.incrementCounter().accountsPartial({
+      signer: provider.wallet.publicKey,
+      counter: counterAccount
+    }).signers([provider.wallet.payer]).rpc();
+
+    console.log(`Transction Signature: ${tx}`);
+  });
+
+  it("Delegate counter", async () => {
+    const tx = await program.methods.delegateCounter().accountsPartial({
+      signer: provider.wallet.publicKey,
+      counter: counterAccount
+    }).transaction();
+
+    const signature = await sendMagicTransaction(
+      routerConnection, 
+      tx, 
+      [provider.wallet.payer],
+    );
+
+    console.log("Delegate signature:", signature);
+
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+  });
+
+  it("COmmit and increment counter", async () => {
+    const tx = await program.methods.commitCounter().accountsPartial({
+      signer: provider.wallet.publicKey,
+      counter: counterAccount
+    }).transaction();
+
+    const signature = await sendMagicTransaction(
+      routerConnection,
+      tx,
+      [provider.wallet.payer],
+    );
+
+    console.log(`Commit signature: ${signature}`);
+
+    await sleepWithAnimation(25);
+
+    const increment_counter_on_chain = await program.methods.incrementCounter().accountsPartial({
+      signer: provider.wallet.publicKey,
+      counter: counterAccount
+    }).signers([provider.wallet.payer]).rpc();
+
+    console.log(`Increment counter signature: ${increment_counter_on_chain}`)
+  })
+});
+
+async function sleepWithAnimation(seconds: number): Promise<void> {
+  const totalMs = seconds * 1000;
+  const interval = 500; // Update every 500ms
+  const iterations = Math.floor(totalMs / interval);
+
+  for (let i = 0; i < iterations; i++) {
+    const dots = '.'.repeat((i % 3) + 1);
+    process.stdout.write(`\rWaiting${dots}   `);
+    await new Promise(resolve => setTimeout(resolve, interval));
+  }
+
+  // Clear the line
+  process.stdout.write('\r\x1b[K');
+}
